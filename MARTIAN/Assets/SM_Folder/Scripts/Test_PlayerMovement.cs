0e52f76e787adf9e63b520564b87604cd52b4c99
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-05-21 12:24:48 +0900

    플레이어 무브 / 카메라 팔로우 프로토 완성본

---
+++ MARTIAN/Assets/SM_Folder/Scripts/Test_PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turnning();
+    }
+
+    private void Turnning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
