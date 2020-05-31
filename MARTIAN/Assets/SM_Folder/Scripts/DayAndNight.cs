c5c0575688b9f461bffa36bb085801158fb58c99
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-06-01 02:38:29 +0900

    낮밤과 헤드라이트 완성

---
+++ MARTIAN/Assets/SM_Folder/Scripts/DayAndNight.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [SerializeField] private float secondPerRealTimeSecond;
+
+    private bool isNight = false;
+
+    [SerializeField] private float fogDensityCalc;
+
+    [SerializeField] private float nightFogDensity;
+    private float dayFogDensity;
+    private float currentFogDensity;
+
+    Test_PlayerMovement tpm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dayFogDensity = RenderSettings.fogDensity;
+        tpm = GameObject.Find("Test_Player").GetComponent<Test_PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecond * Time.deltaTime);
+        if (transform.eulerAngles.x >= 170)
+        {
+            isNight = true;
+            tpm.switchOfTheLight = true;
+            tpm.TurnONOFF();
+        }
+        else if (transform.eulerAngles.x >= -10)
+        {
+            Debug.Log("야 이새꺄");
+            isNight = false;
+            tpm.switchOfTheLight = false;
+            tpm.TurnONOFF();
+        }
+
+        if (isNight)
+        {
+            if(currentFogDensity <= nightFogDensity)
+            {
+                currentFogDensity += 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+        else
+        {
+            Debug.Log("왜 안타 썅");
+            if (currentFogDensity >= dayFogDensity)
+            {
+                currentFogDensity -= 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+    }
+}
